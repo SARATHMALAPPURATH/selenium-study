cde4132ed0e92cb66971c55b7fbfcb33f4a72195
SARATHMALAPPURATH <sarathmalappurath@gmail.com>
2020-12-10 14:34:04 -0500

    Add test for Gender radio buttons. Closes #3

---
+++ src/test/java/com/qaguru/seleniumstudy/tests/WebElementPractice.java
@@ -0,0 +1,43 @@
+package com.qaguru.seleniumstudy.tests;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+public class WebElementPractice {
+    private final String baseUrl = "http://qaguru.ca/webelementapp.php";
+    private WebDriver driver;
+
+    @BeforeTest
+    public void beforeTest(){
+        WebDriverManager.chromedriver().setup();
+        WebDriverManager.firefoxdriver().setup();
+    }
+
+    @BeforeMethod
+    public void setUp(){
+        driver =new ChromeDriver();
+        driver.get(baseUrl);
+
+    }
+    @AfterMethod
+    public void tearDown(){
+        driver.quit();
+
+    }
+
+    @Test
+    public void selectGender(){
+        WebElement radMale= driver.findElement(By.xpath("//*[@id=\"home-5\"]/form[2]/input[1]"));
+        Assert.assertTrue(radMale.isSelected(),"Male radio button is not selected by default");
+       driver.findElement(By.xpath("//*[@id=\"home-5\"]/form[2]/input[2]")).click();
+
+    }
+}
